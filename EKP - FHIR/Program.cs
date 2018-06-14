0be874efbd72b7532dc2a1915b1925f10ec3c0b7
ikami1 <chicken3579@o2.pl>
2018-06-14 15:04:53 +0200

    init commit

---
+++ EKP - FHIR/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
+namespace EKP___FHIR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            connection();
+        }
+
+        public static void connection()
+        {
+            var client = new FhirClient("http://vonk.fire.ly");
+            client.PreferredFormat = ResourceFormat.Json;
+
+            var query = new string[] { "gender=male" };
+            var bundle = client.Search("Patient", query);
+
+            //var bundle = client.Search("Patient");
+
+            Console.WriteLine( "Got " + bundle.Entry.Count() + " records!");
+
+            foreach (var entry in bundle.Entry)
+            {
+                try
+                {
+                    Patient p = (Patient)entry.Resource;
+                    Console.WriteLine(p.Id + " " + p.Name.First().Text + "\r\n");
+                } catch (Exception e) {
+                }
+                
+            }
+
+            Console.WriteLine(bundle.Entry.Count());
+
+        }
+    }
+}
